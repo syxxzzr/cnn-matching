2b323acd2880fa37c2c4498d4eb8b0c73f06796f
syxxzzr <syxxzzr@gmail.com>
2026-04-26 17:10:13 +0800

    refactor: improve feature extraction and matching process
    replace deprecated functions
    reformat the code style

--- cnnmatching.py
+++ cnnmatching.py
@@ -1,103 +1,116 @@
-import argparse
+import sys
+import time
+
 import cv2
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
 import numpy as np
-import imageio
+from skimage import measure, transform
+
 import plotmatch
 from lib.cnn_feature import cnn_feature_extract
-import matplotlib.pyplot as plt
-import time
-from skimage import measure
-from skimage import transform
-
-#time count
-start = time.perf_counter()
 
 _RESIDUAL_THRESHOLD = 30
-#Test1nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8
-imgfile1 = 'df-ms-data/1/df-googleearth-1k-20091227.jpg'
-imgfile2 = 'df-ms-data/1/df-googleearth-1k-20181029.jpg'
-imgfile1 = 'df-ms-data/1/df-uav-sar-500.jpg'
+_DEFAULT_IMAGE_1 = "df-ms-data/1/df-uav-sar-500.jpg"
+_DEFAULT_IMAGE_2 = "df-ms-data/1/df-googleearth-1k-20181029.jpg"
 
 
-start = time.perf_counter()
+def _extract_and_match(image1, image2, start):
+    start0 = time.perf_counter()
 
-# read left image
-image1 = imageio.imread(imgfile1)
-image2 = imageio.imread(imgfile2)
+    kps_left, _, des_left = cnn_feature_extract(image1, nfeatures=-1)
+    kps_right, _, des_right = cnn_feature_extract(image2, nfeatures=-1)
 
-print('read image time is %6.3f' % (time.perf_counter() - start))
+    print(
+        "Feature_extract time is %6.3f, left: %6.3f,right %6.3f"
+        % ((time.perf_counter() - start), len(kps_left), len(kps_right))
+    )
 
-start0 = time.perf_counter()
+    flann = cv2.FlannBasedMatcher(
+        dict(algorithm=1, trees=5),
+        dict(checks=40),
+    )
+    knn_matches = flann.knnMatch(des_left, des_right, k=2)
 
-kps_left, sco_left, des_left = cnn_feature_extract(image1,  nfeatures = -1)
-kps_right, sco_right, des_right = cnn_feature_extract(image2,  nfeatures = -1)
+    good_matches = []
+    locations_1_to_use = []
+    locations_2_to_use = []
 
-print('Feature_extract time is %6.3f, left: %6.3f,right %6.3f' % ((time.perf_counter() - start), len(kps_left), len(kps_right)))
-start = time.perf_counter()
+    distance_diff_avg = 0
+    for match_1, match_2 in knn_matches:
+        distance_diff_avg += match_2.distance - match_1.distance
+    distance_diff_avg = distance_diff_avg / len(knn_matches)
 
-#Flann特征匹配
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-search_params = dict(checks=40)
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des_left, des_right, k=2)
+    for match_1, match_2 in knn_matches:
+        if match_2.distance > match_1.distance + distance_diff_avg:
+            good_matches.append(match_1)
+            point_2 = cv2.KeyPoint(
+                kps_right[match_1.trainIdx][0],
+                kps_right[match_1.trainIdx][1],
+                1,
+            )
+            point_1 = cv2.KeyPoint(
+                kps_left[match_1.queryIdx][0],
+                kps_left[match_1.queryIdx][1],
+                1,
+            )
+            locations_1_to_use.append([point_1.pt[0], point_1.pt[1]])
+            locations_2_to_use.append([point_2.pt[0], point_2.pt[1]])
 
-goodMatch = []
-locations_1_to_use = []
-locations_2_to_use = []
+    print("match num is %d" % len(good_matches))
 
-# 匹配对筛选
-min_dist = 1000
-max_dist = 0
-disdif_avg = 0
-# 统计平均距离差
-for m, n in matches:
-    disdif_avg += n.distance - m.distance
-disdif_avg = disdif_avg / len(matches)
+    locations_1_to_use = np.array(locations_1_to_use)
+    locations_2_to_use = np.array(locations_2_to_use)
 
-for m, n in matches:
-    #自适应阈值
-    if n.distance > m.distance + disdif_avg:
-        goodMatch.append(m)
-        p2 = cv2.KeyPoint(kps_right[m.trainIdx][0],  kps_right[m.trainIdx][1],  1)
-        p1 = cv2.KeyPoint(kps_left[m.queryIdx][0], kps_left[m.queryIdx][1], 1)
-        locations_1_to_use.append([p1.pt[0], p1.pt[1]])
-        locations_2_to_use.append([p2.pt[0], p2.pt[1]])
-#goodMatch = sorted(goodMatch, key=lambda x: x.distance)
-print('match num is %d' % len(goodMatch))
-locations_1_to_use = np.array(locations_1_to_use)
-locations_2_to_use = np.array(locations_2_to_use)
+    _, inliers = measure.ransac(
+        (locations_1_to_use, locations_2_to_use),
+        transform.AffineTransform,
+        min_samples=3,
+        residual_threshold=_RESIDUAL_THRESHOLD,
+        max_trials=1000,
+    )
+    print("Found %d inliers" % sum(inliers))
 
-# Perform geometric verification using RANSAC.
-_, inliers = measure.ransac((locations_1_to_use, locations_2_to_use),
-                          transform.AffineTransform,
-                          min_samples=3,
-                          residual_threshold=_RESIDUAL_THRESHOLD,
-                          max_trials=1000)
+    inlier_idxs = np.nonzero(inliers)[0]
+    matches = np.column_stack((inlier_idxs, inlier_idxs))
+    print("whole time is %6.3f" % (time.perf_counter() - start0))
 
-print('Found %d inliers' % sum(inliers))
+    return locations_1_to_use, locations_2_to_use, matches
 
-inlier_idxs = np.nonzero(inliers)[0]
-#最终匹配结果
-matches = np.column_stack((inlier_idxs, inlier_idxs))
-print('whole time is %6.3f' % (time.perf_counter() - start0))
 
-# Visualize correspondences, and save to file.
-#1 绘制匹配连线
-plt.rcParams['savefig.dpi'] = 100 #图片像素
-plt.rcParams['figure.dpi'] = 100 #分辨率
-plt.rcParams['figure.figsize'] = (4.0, 3.0) # 设置figure_size尺寸
-_, ax = plt.subplots()
-plotmatch.plot_matches(
-    ax,
-    image1,
-    image2,
-    locations_1_to_use,
-    locations_2_to_use,
-    np.column_stack((inlier_idxs, inlier_idxs)),
-    plot_matche_points = False,
-    matchline = True,
-    matchlinewidth = 0.3)
-ax.axis('off')
-ax.set_title('')
-plt.show()
+def _show_matches(image1, image2, locations_1_to_use, locations_2_to_use, matches):
+    plt.rcParams["savefig.dpi"] = 100
+    plt.rcParams["figure.dpi"] = 100
+    plt.rcParams["figure.figsize"] = (4.0, 3.0)
+
+    _, axis = plt.subplots()
+    plotmatch.plot_matches(
+        axis,
+        image1,
+        image2,
+        locations_1_to_use,
+        locations_2_to_use,
+        matches,
+        plot_matche_points=False,
+        matchline=True,
+        matchlinewidth=0.3,
+    )
+    axis.axis("off")
+    axis.set_title("")
+    plt.show()
+
+
+def main(imgfile1=_DEFAULT_IMAGE_1, imgfile2=_DEFAULT_IMAGE_2):
+    start = time.perf_counter()
+    image1 = imageio.imread(imgfile1)
+    image2 = imageio.imread(imgfile2)
+    print("read image time is %6.3f" % (time.perf_counter() - start))
+
+    locations_1_to_use, locations_2_to_use, matches = _extract_and_match(
+        image1, image2, start
+    )
+    _show_matches(image1, image2, locations_1_to_use, locations_2_to_use, matches)
+
+
+if __name__ == "__main__":
+    main()
--- lib/cnn_feature.py
+++ lib/cnn_feature.py
@@ -1,87 +1,113 @@
-import torch
-from lib.model import D2Net
-from lib.utils import preprocess_image
-from lib.pyramid import process_multiscale
-import scipy
-import scipy.io
-import scipy.misc
+import cv2
 import numpy as np
+import torch
 
-use_cuda = torch.cuda.is_available()
+from lib.model import D2Net
+from lib.pyramid import process_multiscale
+from lib.utils import preprocess_image
 
-# Creating CNN model
-model = D2Net(
-    model_file="models/d2_tf.pth",
-    use_relu=True,
-    use_cuda=use_cuda
-)
-device = torch.device("cuda:0" if use_cuda else "cpu")
+_MODEL_FILE = "models/d2_tf.pth"
+_DEFAULT_SCALES = [0.25, 0.50, 1.0]
+_MAX_EDGE = 2500
+_MAX_SUM_EDGES = 5000
 
-multiscale = True
-max_edge = 2500
-max_sum_edges = 5000
-# de-net feature extract function
-def cnn_feature_extract(image,scales=[.25, 0.50, 1.0], nfeatures = 1000):
+
+def _resolve_device(device=None):
+    if device is None:
+        return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if isinstance(device, str):
+        return torch.device(device)
+    return device
+
+
+def create_feature_extractor(device=None):
+    target_device = _resolve_device(device)
+    extractor = D2Net(
+        model_file=_MODEL_FILE,
+        use_relu=True,
+        use_cuda=(target_device.type == "cuda"),
+    )
+    extractor.eval()
+    if target_device.type == "cuda":
+        extractor = extractor.to(target_device)
+    return extractor, target_device
+
+
+_DEFAULT_MODEL, _DEFAULT_DEVICE = create_feature_extractor()
+
+
+def _resize_image(image, scale):
+    resized_height = max(1, int(image.shape[0] * scale))
+    resized_width = max(1, int(image.shape[1] * scale))
+    return cv2.resize(
+        image,
+        (resized_width, resized_height),
+        interpolation=cv2.INTER_LINEAR,
+    ).astype("float")
+
+
+def _extract_with_model(image, model, device, scales, nfeatures):
     if len(image.shape) == 2:
         image = image[:, :, np.newaxis]
         image = np.repeat(image, 3, -1)
 
-    # TODO: switch to PIL.Image due to deprecation of scipy.misc.imresize.
     resized_image = image
-    if max(resized_image.shape) > max_edge:
-        resized_image = scipy.misc.imresize(
-            resized_image,
-            max_edge / max(resized_image.shape)
-        ).astype('float')
-    if sum(resized_image.shape[: 2]) > max_sum_edges:
-        resized_image = scipy.misc.imresize(
-            resized_image,
-            max_sum_edges / sum(resized_image.shape[: 2])
-        ).astype('float')
+    if max(resized_image.shape) > _MAX_EDGE:
+        resized_image = _resize_image(
+            resized_image, _MAX_EDGE / max(resized_image.shape)
+        )
+    if sum(resized_image.shape[:2]) > _MAX_SUM_EDGES:
+        resized_image = _resize_image(
+            resized_image, _MAX_SUM_EDGES / sum(resized_image.shape[:2])
+        )
 
     fact_i = image.shape[0] / resized_image.shape[0]
     fact_j = image.shape[1] / resized_image.shape[1]
 
-    input_image = preprocess_image(
-        resized_image,
-        preprocessing="torch"
-    )
+    input_image = preprocess_image(resized_image, preprocessing="torch")
     with torch.no_grad():
-        if multiscale:
-            keypoints, scores, descriptors = process_multiscale(
-                torch.tensor(
-                    input_image[np.newaxis, :, :, :].astype(np.float32),
-                    device=device
-                ),
-                model,
-                scales
-            )
-        else:
-            keypoints, scores, descriptors = process_multiscale(
-                torch.tensor(
-                    input_image[np.newaxis, :, :, :].astype(np.float32),
-                    device=device
-                ),
-                model,
-                scales
-            )
+        keypoints, scores, descriptors = process_multiscale(
+            torch.tensor(
+                input_image[np.newaxis, :, :, :].astype(np.float32),
+                device=device,
+            ),
+            model,
+            scales,
+        )
 
-    # Input image coordinates
     keypoints[:, 0] *= fact_i
     keypoints[:, 1] *= fact_j
-    # i, j -> u, v
     keypoints = keypoints[:, [1, 0, 2]]
 
     if nfeatures != -1:
-        #根据scores排序
         scores2 = np.array([scores]).T
         res = np.hstack((scores2, keypoints))
         res = res[np.lexsort(-res[:, ::-1].T)]
 
         res = np.hstack((res, descriptors))
-        #取前几个
         scores = res[0:nfeatures, 0].copy()
         keypoints = res[0:nfeatures, 1:4].copy()
         descriptors = res[0:nfeatures, 4:].copy()
         del res
+
     return keypoints, scores, descriptors
+
+
+def cnn_feature_extract_with_extractor(
+    image,
+    model,
+    device,
+    scales=None,
+    nfeatures=1000,
+):
+    if scales is None:
+        scales = _DEFAULT_SCALES
+    return _extract_with_model(image, model, device, scales, nfeatures)
+
+
+def cnn_feature_extract(image, scales=None, nfeatures=1000):
+    if scales is None:
+        scales = _DEFAULT_SCALES
+    return _extract_with_model(
+        image, _DEFAULT_MODEL, _DEFAULT_DEVICE, scales, nfeatures
+    )
