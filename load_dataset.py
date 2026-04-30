5edf178e110bf267a90278e3937640fe3805bcef
syxxzzr <syxxzzr@gmail.com>
2026-04-30 16:53:43 +0800

    fix(OSdataset2 loader): fix the wrong value of image count

--- load_dataset.py
+++ load_dataset.py
@@ -28,7 +28,6 @@ def load_QXSLAB_SAROPT(img_path_1 = _QXSLAB_SAROPT_TEST_IMG_PATH_1, img_path_2 =
 _OSdataset2_TEST_IMG_PATH = r'E:\cmm_proj\OSDataset2.0\Patch-level Subset\OSdataset\512\val'
 
 def _yield_OSdataset2_img(dataset_path, count):
-
     for i in range(1, count + 1):
         try:
             image_1 = imageio.imread(os.path.join(dataset_path, f'opt{i}.png'))
@@ -41,4 +40,5 @@ def _yield_OSdataset2_img(dataset_path, count):
 
 def load_OSdataset2(dataset_path = _OSdataset2_TEST_IMG_PATH):
     image_list = os.listdir(dataset_path)
-    return len(image_list), lambda : _yield_OSdataset2_img(dataset_path, len(image_list))
+    image_count = int(len(image_list) / 2)
+    return image_count, lambda : _yield_OSdataset2_img(dataset_path, image_count)
