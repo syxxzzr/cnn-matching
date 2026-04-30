ede6e437d55f9f451aeefd46bba5675ad142dfbf
syxxzzr <syxxzzr@gmail.com>
2026-04-30 16:49:57 +0800

    feat: implement loading functionality for OSdataset 2.0

--- load_dataset.py
+++ load_dataset.py
@@ -24,4 +24,21 @@ def load_QXSLAB_SAROPT(img_path_1 = _QXSLAB_SAROPT_TEST_IMG_PATH_1, img_path_2 =
     img_list = list(set(os.listdir(img_path_1)) & set(os.listdir(img_path_2)))
     return len(img_list), lambda : _yield_QXSLAB_SAROPT_img(img_path_1, img_path_2, img_list)
 
-""" Load """
+""" Load OSdataset 2.0 dataset """
+_OSdataset2_TEST_IMG_PATH = r'E:\cmm_proj\OSDataset2.0\Patch-level Subset\OSdataset\512\val'
+
+def _yield_OSdataset2_img(dataset_path, count):
+
+    for i in range(1, count + 1):
+        try:
+            image_1 = imageio.imread(os.path.join(dataset_path, f'opt{i}.png'))
+            image_2 = imageio.imread(os.path.join(dataset_path, f'sar{i}.png'))
+        except:
+            image_1 = np.array([])
+            image_2 = np.array([])
+
+        yield f'opt{i}.png', image_1, image_2, None
+
+def load_OSdataset2(dataset_path = _OSdataset2_TEST_IMG_PATH):
+    image_list = os.listdir(dataset_path)
+    return len(image_list), lambda : _yield_OSdataset2_img(dataset_path, len(image_list))
